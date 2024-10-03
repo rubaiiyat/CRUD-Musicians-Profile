9c712b514c7f611c991c62c55779510bdea5f0e0
rubaiiyat <abirr1516@gmail.com>
2024-10-03 17:01:18 +0600

    Register and Login completed

--- Musicians/settings.py
+++ Musicians/settings.py
@@ -39,7 +39,12 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "albums",
     "profiles",
+    "crispy_forms",
+    "crispy_tailwind",
 ]
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+
+CRISPY_TEMPLATE_PACK = "tailwind"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
--- profiles/views.py
+++ profiles/views.py
@@ -1,14 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . import forms
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def register(request):
     page = "Register"
-    return render(request, "register.html", {"page": page})
+    if request.method == "POST":
+        form = forms.userRegistration(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("register")
+    else:
+        form = forms.userRegistration()
+    return render(request, "register.html", {"page": page, "form": form})
 
 
 def userLogin(request):
-    page = "Login"
-    return render(request, "register.html", {"page": page})
+    if request.user.is_authenticated:
+        return redirect("home")
+    else:
+        page = "Login"
+        if request.method == "POST":
+            form = AuthenticationForm(request, data=request.POST)
+            if form.is_valid():
+                name = form.cleaned_data.get("username")
+                userPass = form.cleaned_data.get("password")
+                user = authenticate(request, username=name, password=userPass)
+                if user is not None:
+                    form = login(request, user)
+                    return redirect("home")
+        else:
+            form = AuthenticationForm()
+        return render(request, "register.html", {"page": page, "form": form})
 
 
 def profiles(request):
