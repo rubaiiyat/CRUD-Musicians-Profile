7325f88a88af31491ecb72de02e321018c1e0e15
rubaiiyat <abirr1516@gmail.com>
2024-10-05 23:28:31 +0600

    Login And Profile done with view class

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path("auth/register/", views.register.as_view(), name="register"),
-    path("auth/login/", views.userLogin, name="login"),
+    path("auth/login/", views.userLogin.as_view(), name="login"),
     path("auth/editprofile/", views.editProfile, name="editprofile"),
     path("auth/changepassword/", views.chngPassword, name="changepassword"),
-    path("", views.profiles, name="profile"),
+    path("", views.profiles.as_view(), name="profile"),
     path("auth/logout/", views.userLogout, name="logout"),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -4,7 +4,8 @@ from django.contrib.auth import login, logout, authenticate, update_session_auth
 from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 
 
@@ -12,6 +13,11 @@ class register(CreateView):
     form_class = forms.userRegistration
     template_name = "register.html"
     success_url = reverse_lazy("register")
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect("profile")
+        return super().dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -24,35 +30,46 @@ class register(CreateView):
         return super().form_valid(form)
 
 
-def userLogin(request):
-    if request.user.is_authenticated:
-        return redirect("profile")
-    else:
-        page = "Login"
-        if request.method == "POST":
-            form = AuthenticationForm(request, data=request.POST)
-            if form.is_valid():
-                name = form.cleaned_data.get("username")
-                userPass = form.cleaned_data.get("password")
-                user = authenticate(request, username=name, password=userPass)
-                if user is not None:
-                    messages.success(request, "Logged in Successfully")
-                    form = login(request, user)
-                    return redirect("profile")
-            messages.warning(request, "Invalid Username or Password")
-        else:
-            form = AuthenticationForm()
-        return render(request, "register.html", {"page": page, "form": form})
+class userLogin(LoginView):
+    template_name = "register.html"
+
+    def dispatch(self, request, *args, **kwargs):
+
+        if self.request.user.is_authenticated:
+            return redirect("profile")
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_success_url(self) -> str:
+        return reverse_lazy("profile")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page"] = "Login"
+        return context
+
+    def form_valid(self, form):
+        messages.success(self.request, "Logged in Successfully")
+
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.warning(self.request, "Invalid Username or Password")
+        return super().form_invalid(form)
 
 
-def profiles(request):
-    if request.user.is_authenticated:
-        page = "Profile"
-        profile_data = User.objects.get(username=request.user)
+class profiles(TemplateView):
+    template_name = "profile.html"
 
-        return render(request, "profile.html", {"page": page, "data": profile_data})
-    else:
-        return redirect("login")
+    def dispatch(self, request, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return redirect("login")
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page"] = "User Profile"
+        context["data"] = self.request.user
+        return context
 
 
 def editProfile(request):
